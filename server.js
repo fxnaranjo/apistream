8e8d4dcd54d1833bbba6b884ecc9d3fc16fcc861
Francisco Naranjo <fxnaranjo@gmail.com>
2022-12-11 00:38:38 -0500

    tls api

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express'),
+  fs = require('fs-extra'),
   app = express(),
   port = process.env.PORT || 3000,
   bodyParser = require('body-parser');
-  
 
+const https = require("https");
+  
+  var privateKey = fs.readFileSync( '/certificates/privkey.pem' );
+  var certificate = fs.readFileSync( '/certificates/fullchain.pem' );
+  
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,7 +18,11 @@ var routes = require('./api/routes/streamingRoutes'); //importing route
 routes(app); //register the route
 
 
-app.listen(3000, '0.0.0.0');
+https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app).listen(port,'0.0.0.0');
+
 
 
 console.log('todo list RESTful API server started on: ' + port);
